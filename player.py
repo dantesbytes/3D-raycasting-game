ad3633b358c1c30ffbe9d135866bf080919dfe6b
dantesbytes <dantesbytes.dev@yahoo.com>
2024-09-25 22:23:27 -0400

    main, player

---
+++ player.py
@@ -0,0 +1,47 @@
+from settings import *
+import pygame as pg
+import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+        
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time 
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+        
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_sin
+            dy += speed_cos
+        if keys[pg.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+            
+            
+        self.x += dxself.y += dy
+            
+        
+    
+    def update(self):
+        self.movement()
+        
+    @property
+    def pos(self):
+        return self.x, self.y
+    
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
--- settings.py
+++ settings.py
@@ -1,2 +1,10 @@
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
+
+#player, movement, rotation
+
+
+PLAYER_POS = 1.5, 5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.0004 
+PLAYER_ROT_SPEED = 0.002
